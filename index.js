17d7f0645e5bb32523f24b1717ab023de39496ca
Thomas Culbertson <97178572+KaosSpectre@users.noreply.github.com>
2022-10-15 21:09:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+];
+let passwordLength = 15;
+const pass1El = document.getElementById("pass1");
+const pass2El = document.getElementById("pass2");
+const btnEl = document.getElementById("generate");
+
+function generate() {
+	let password = "";
+	for (let i = 0; i < passwordLength; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		password += characters[randomIndex];
+	}
+	return password;
+}
+
+function getPassword() {
+	pass1El.textContent = generate();
+	pass2El.textContent = generate();
+}
+
+function reset() {
+	pass1El.textContent = "Password 1";
+	pass2El.textContent = "Password 2";
+}
